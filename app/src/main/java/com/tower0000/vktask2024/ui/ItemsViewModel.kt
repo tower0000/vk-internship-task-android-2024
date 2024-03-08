78fe6231d2d0e927b7432a9386f46139d440d71d
tower0000 <veselovml@yandex.ru>
2024-03-08 13:46:48 +0300

    feat: items RV, item view

--- app/src/main/java/com/tower0000/vktask2024/ui/ItemsViewModel.kt
+++ app/src/main/java/com/tower0000/vktask2024/ui/ItemsViewModel.kt
@@ -1,4 +1,6 @@
 package com.tower0000.vktask2024.ui
 
-class ItemsViewModel {
+import androidx.lifecycle.ViewModel
+
+class ItemsViewModel: ViewModel() {
 }
--- app/src/main/java/com/tower0000/vktask2024/ui/MainActivity.kt
+++ app/src/main/java/com/tower0000/vktask2024/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.tower0000.vktask2024.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.tower0000.vktask2024.R
+import com.tower0000.vktask2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.itemsHostFragment) as NavHostFragment
     }
 }
